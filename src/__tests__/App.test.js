961a2f70074cc0a9ef34a6c865ab087e274772c4
StuartCowley <voidstalker@live.co.uk>
2023-01-06 17:53:05 +0000

    Added heading test for app component and created title

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -3,10 +3,10 @@ import { render, screen } from "@testing-library/react"
 import App from "../components/App/App"
 
 describe("App component", () => {
-  it("Renders title", () => {
+  it("Renders single heading with correct text", () => {
     render(<App />)
-    const linkElement = screen.getByText(/some title/i)
 
-    expect(linkElement).toBeInTheDocument()
+    expect(screen.getAllByRole("heading", { level: 1 })).toHaveLength(1)
+    expect(screen.getByRole("heading", { name: "Pomodoro timer" }))
   })
 })
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,7 +2,11 @@ import React from "react"
 import "./app.css"
 
 const App = () => {
-  return <div className="app" />
+  return (
+    <div className="app">
+      <h1>Pomodoro timer</h1>
+    </div>
+  )
 }
 
 export default App
