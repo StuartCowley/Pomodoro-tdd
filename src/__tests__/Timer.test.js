9b74fea26ddb3dcd362ef829bb17b31122fac3d4
StuartCowley <voidstalker@live.co.uk>
2023-01-09 17:11:15 +0000

    Added describe block to Timer test, test function prop is called in TimerButton

--- src/__tests__/Timer.test.js
+++ src/__tests__/Timer.test.js
@@ -2,14 +2,16 @@ import React from "react"
 import { render, screen } from "@testing-library/react"
 import Timer from "../components/Timer/Timer"
 
-it("Renders single heading with correct text", () => {
-  render(<Timer />)
+describe("Timer component", () => {
+  it("Renders single heading with correct text", () => {
+    render(<Timer />)
 
-  expect(screen.getByTestId("timerContainer")).toBeInTheDocument()
-})
+    expect(screen.getByTestId("timerContainer")).toBeInTheDocument()
+  })
 
-it("Should render instances of the TimerButton component", () => {
-  render(<Timer />)
+  it("Should render instances of the TimerButton component", () => {
+    render(<Timer />)
 
-  expect(screen.getAllByTestId("timerButtonContainer")).toHaveLength(3)
+    expect(screen.getAllByTestId("timerButtonContainer")).toHaveLength(3)
+  })
 })
--- src/__tests__/TimerButton.test.js
+++ src/__tests__/TimerButton.test.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { render, screen } from "@testing-library/react"
+import { fireEvent, render, screen } from "@testing-library/react"
 import TimerButton from "../components/TimerButton/TimerButton"
 
 describe("TimerButton", () => {
@@ -18,5 +18,17 @@ describe("TimerButton", () => {
 
     expect(screen.getByTestId("timerButtonContainer")).toBeInTheDocument()
     expect(screen.getByText(validProps.buttonValue)).toBeInTheDocument()
+  })
+
+  it("Function is called when button is clicked", () => {
+    render(
+      <TimerButton
+        buttonAction={validProps.buttonAction}
+        buttonValue={validProps.buttonValue}
+      />,
+    )
+
+    fireEvent.click(screen.getByText("Some label"))
+    expect(validProps.buttonAction).toHaveBeenCalledTimes(1)
   })
 })
