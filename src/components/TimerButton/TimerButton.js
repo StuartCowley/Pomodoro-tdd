152ed58920b084d7f5e929287652e4b17fd5c1ba
StuartCowley <voidstalker@live.co.uk>
2023-01-09 15:58:09 +0000

    Added timer-button.css, refactored classnames in other files

--- src/components/TimerButton/TimerButton.js
+++ src/components/TimerButton/TimerButton.js
@@ -1,10 +1,15 @@
 import React from "react"
 import PropTypes from "prop-types"
+import "./timer-button.css"
 
 const TimerButton = ({ buttonAction, buttonValue }) => {
   return (
-    <div onClick={buttonAction} data-testid="timerButtonContainer">
-      {buttonValue}
+    <div
+      className="button__container"
+      onClick={buttonAction}
+      data-testid="timerButtonContainer"
+    >
+      <p className="button__value">{buttonValue}</p>
     </div>
   )
 }
