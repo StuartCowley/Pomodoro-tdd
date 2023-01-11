095a0a627c4077746bc08210a47d7041e55479e0
StuartCowley <voidstalker@live.co.uk>
2023-01-11 12:52:07 +0000

    Added timer functionality to component

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useEffect, useRef, useState } from "react"
 import TimerButton from "../TimerButton/TimerButton"
 import "./timer.css"
 
@@ -10,23 +10,70 @@ const Timer = () => {
   }
   const [timerData, setTimerData] = useState(initialState)
 
+  // custom hook
+  function useInterval(callback, delay) {
+    const savedCallback = useRef()
+
+    // Remember the latest callback.
+    useEffect(() => {
+      savedCallback.current = callback
+    }, [callback])
+
+    // Set up the interval.
+    useEffect(() => {
+      function tick() {
+        savedCallback.current()
+      }
+      if (timerData.isOn) {
+        let id = setInterval(tick, delay)
+        return () => clearInterval(id)
+      }
+    }, [timerData.isOn])
+  }
+
+  useInterval(() => {
+    if (timerData.seconds > 0) {
+      setTimerData({ ...timerData, seconds: timerData.seconds - 1 })
+    }
+    if (timerData.seconds === 0) {
+      if (timerData.minutes === 0) {
+        setTimerData({ ...timerData, isOn: false })
+      } else {
+        setTimerData({
+          ...timerData,
+          minutes: timerData.minutes - 1,
+          seconds: 59,
+        })
+      }
+    }
+  }, 1000)
+
+  const displayedTime = `${
+    timerData.minutes >= 10 ? timerData.minutes : `0${timerData.minutes}`
+  }:${timerData.seconds >= 10 ? timerData.seconds : `0${timerData.seconds}`}`
+
   const startTimer = () => {
-    console.log("Starting timer.")
+    setTimerData({ ...timerData, isOn: true })
   }
 
   const stopTimer = () => {
-    console.log("Stopping timer.")
+    setTimerData({ ...timerData, isOn: false })
   }
 
   const resetTimer = () => {
-    console.log("Resetting timer.")
+    setTimerData(initialState)
   }
 
   return (
     <div className="timer__container" data-testid="timerContainer">
-      <TimerButton buttonAction={startTimer} buttonValue="Start" />
-      <TimerButton buttonAction={stopTimer} buttonValue="Stop" />
-      <TimerButton buttonAction={resetTimer} buttonValue="Reset" />
+      <div className="timer__time-display" data-testid="timeDisplayContainer">
+        {displayedTime}
+      </div>
+      <div className="timer__button-wrap">
+        <TimerButton buttonAction={startTimer} buttonValue="Start" />
+        <TimerButton buttonAction={stopTimer} buttonValue="Stop" />
+        <TimerButton buttonAction={resetTimer} buttonValue="Reset" />
+      </div>
     </div>
   )
 }
